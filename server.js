16a7765259b500f51e0e96de812140c752a2e34c
RevitalS <revitalsirotkin@h-MacBook-Pro-sl-Revital.local>
2020-07-07 13:33:23 +0300

    change to heroku db

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const image = require('./controllers/image');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//     connectionString : process.env.DATABASE_URL,
-//     ssl: true
-//   }
-// });
-
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'revitalsirotkin',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'revitalsirotkin',
+//     password : '',
+//     database : 'smart-brain'
+//   }
+// });
 
 const app = express();
 app.use(express.json());
